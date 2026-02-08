23132aa6e987f7bf3e03b2810f96442e3edd2c8d
AenusMcPlough <frank.young2005@yahoo.com>
2026-02-08 23:42:47 +0000

    finishing BracketChecker

--- src/project20280/stacksqueues/BracketChecker.java
+++ src/project20280/stacksqueues/BracketChecker.java
@@ -8,7 +8,28 @@ class BracketChecker {
     }
 
     public void check() {
-        // TODO
+        LinkedStack<Character> stack = new LinkedStack<>();
+        char closed = '_';
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+
+                }
+            else if(c == ')' || c == '}' || c == ']'){
+                if (stack.isEmpty()){System.out.println("not correct; nothing matches final " + c + "\n"); return;}
+                char popped = stack.pop();
+                if( popped == '{'){closed = '}';}
+                else if (popped == '['){closed = ']';}
+                else if (popped == '('){closed = ')';}
+                if (closed != c){
+                    System.out.println("not correct; " + c +  "doesn't match " + popped + " \n");
+                    return;
+                }
+                }
+        }
+        if(stack.isEmpty()){System.out.println("correct\n");}
+        else{System.out.println("not correct; Nothing matches opening " + stack.pop());}
     }
 
     public static void main(String[] args) {
